eaa65d4aa7d05d2b7633f326607a822e03e95dbd
GreatSum <42463375+oguzkaganaltas@users.noreply.github.com>
2022-04-25 10:12:24 +0300

    first commit

---
+++ src/main/java/com/oguzkaganmustafa/festivalmanager/FestivalmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.oguzkaganmustafa.festivalmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivalmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestivalmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oguzkaganmustafa/festivalmanager/FestivalmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oguzkaganmustafa.festivalmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestivalmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
