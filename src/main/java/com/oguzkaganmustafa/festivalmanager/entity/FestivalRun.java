bb0ed55e117bcadac434aad237389ab0a229d5c4
GreatSum <42463375+oguzkaganaltas@users.noreply.github.com>
2022-04-27 18:52:59 +0300

    embedded id weak key removed

--- src/main/java/com/oguzkaganmustafa/festivalmanager/entity/FestivalRun.java
+++ src/main/java/com/oguzkaganmustafa/festivalmanager/entity/FestivalRun.java
@@ -13,8 +13,6 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class FestivalRun {
-    @EmbeddedId
-    private FestivalRunId festivalRunId;
 
     private String duration;
 
