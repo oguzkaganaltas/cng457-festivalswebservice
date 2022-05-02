23e52f5a3e158c2b9fead7fc7f84119d3e7b6a2b
Dexess <aygunmustafa999@gmail.com>
2022-05-03 00:11:18 +0300

    AddFestivalRun and AddConcert implemented

--- src/main/java/com/oguzkaganmustafa/festivalmanager/service/ConcertService.java
+++ src/main/java/com/oguzkaganmustafa/festivalmanager/service/ConcertService.java
@@ -1,5 +1,6 @@
 package com.oguzkaganmustafa.festivalmanager.service;
 
+import com.oguzkaganmustafa.festivalmanager.entity.Concert;
 import com.oguzkaganmustafa.festivalmanager.repository.ConcertRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,5 +10,9 @@ public class ConcertService {
 
     @Autowired
     ConcertRepository concertRepository;
+
+    public Concert saveConcert(Concert concert){
+        return concertRepository.save(concert);
+    }
 
 }
