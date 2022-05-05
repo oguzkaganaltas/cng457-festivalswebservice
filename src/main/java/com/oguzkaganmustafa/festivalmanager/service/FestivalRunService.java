9f969ffb2ae18acb25907a1036852832ff82d035
GreatSum <42463375+oguzkaganaltas@users.noreply.github.com>
2022-05-05 15:28:16 +0300

    imports

--- src/main/java/com/oguzkaganmustafa/festivalmanager/service/FestivalRunService.java
+++ src/main/java/com/oguzkaganmustafa/festivalmanager/service/FestivalRunService.java
@@ -5,6 +5,8 @@ import com.oguzkaganmustafa.festivalmanager.repository.FestivalRunRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class FestivalRunService {
 
