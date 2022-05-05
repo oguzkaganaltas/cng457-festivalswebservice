493ac10583ed41bed6f8d3ee967dc1ba2069f53d
GreatSum <42463375+oguzkaganaltas@users.noreply.github.com>
2022-05-05 15:19:20 +0300

    imports

--- src/main/java/com/oguzkaganmustafa/festivalmanager/entity/Festival.java
+++ src/main/java/com/oguzkaganmustafa/festivalmanager/entity/Festival.java
@@ -1,5 +1,7 @@
 package com.oguzkaganmustafa.festivalmanager.entity;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
