66be04ab035f2eee182542cdbddac7c05f7309df
GreatSum <42463375+oguzkaganaltas@users.noreply.github.com>
2022-05-05 15:27:56 +0300

    custom selection by foreign key of festival
    using festival key aas a forign key in festivalrun, find all festivalruns containing the given festival id

--- src/main/java/com/oguzkaganmustafa/festivalmanager/repository/FestivalRunRepository.java
+++ src/main/java/com/oguzkaganmustafa/festivalmanager/repository/FestivalRunRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface FestivalRunRepository extends JpaRepository<FestivalRun, Integer> {
+    List<FestivalRun> findByFestival_FestivalId(int festId);
 }
