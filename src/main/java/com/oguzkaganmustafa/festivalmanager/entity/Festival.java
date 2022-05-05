7d33321758bf5cb5716c631e15dbd78ba8acf203
GreatSum <42463375+oguzkaganaltas@users.noreply.github.com>
2022-05-05 15:26:06 +0300

    Jsonidentiffy Info added

--- src/main/java/com/oguzkaganmustafa/festivalmanager/entity/Festival.java
+++ src/main/java/com/oguzkaganmustafa/festivalmanager/entity/Festival.java
@@ -13,6 +13,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "festivalId")
 public class Festival {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
