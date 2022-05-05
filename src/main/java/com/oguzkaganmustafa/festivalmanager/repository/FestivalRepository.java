f7495e3842b0db438e0707accae6a54342ba3400
GreatSum <42463375+oguzkaganaltas@users.noreply.github.com>
2022-05-05 15:26:57 +0300

    custom search by place implemented

--- src/main/java/com/oguzkaganmustafa/festivalmanager/repository/FestivalRepository.java
+++ src/main/java/com/oguzkaganmustafa/festivalmanager/repository/FestivalRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface FestivalRepository extends JpaRepository<Festival, Integer> {
+    List<Festival> findByPlace(String city);
 }
