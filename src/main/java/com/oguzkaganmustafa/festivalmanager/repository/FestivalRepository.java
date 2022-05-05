e3ebe9ccf2a09b8c80b5c903b849d91bf4b50794
GreatSum <42463375+oguzkaganaltas@users.noreply.github.com>
2022-05-05 15:26:41 +0300

    import

--- src/main/java/com/oguzkaganmustafa/festivalmanager/repository/FestivalRepository.java
+++ src/main/java/com/oguzkaganmustafa/festivalmanager/repository/FestivalRepository.java
@@ -3,5 +3,7 @@ package com.oguzkaganmustafa.festivalmanager.repository;
 import com.oguzkaganmustafa.festivalmanager.entity.Festival;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FestivalRepository extends JpaRepository<Festival, Integer> {
 }
