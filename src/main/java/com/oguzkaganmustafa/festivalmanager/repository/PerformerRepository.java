64777bb493d6e8181d62eb42431040271ff78271
GreatSum <42463375+oguzkaganaltas@users.noreply.github.com>
2022-05-08 20:48:53 +0300

    key type changed to integer

--- src/main/java/com/oguzkaganmustafa/festivalmanager/repository/PerformerRepository.java
+++ src/main/java/com/oguzkaganmustafa/festivalmanager/repository/PerformerRepository.java
@@ -1,8 +1,7 @@
 package com.oguzkaganmustafa.festivalmanager.repository;
 
 import com.oguzkaganmustafa.festivalmanager.entity.Performer;
-import com.oguzkaganmustafa.festivalmanager.helper.PerformerID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PerformerRepository extends JpaRepository<Performer, PerformerID> {
+public interface PerformerRepository extends JpaRepository<Performer, Integer> {
 }
