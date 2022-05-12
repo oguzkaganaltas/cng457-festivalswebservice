019c3ec41c4aab2d3a9f4eabaec68ce57f4c3e85
GreatSum <42463375+oguzkaganaltas@users.noreply.github.com>
2022-05-12 22:54:24 +0300

    Update OrganiserService.java

--- src/main/java/com/oguzkaganmustafa/festivalmanager/service/OrganiserService.java
+++ src/main/java/com/oguzkaganmustafa/festivalmanager/service/OrganiserService.java
@@ -1,6 +1,8 @@
 package com.oguzkaganmustafa.festivalmanager.service;
 
+import com.oguzkaganmustafa.festivalmanager.entity.FestivalRun;
 import com.oguzkaganmustafa.festivalmanager.entity.Organiser;
+import com.oguzkaganmustafa.festivalmanager.repository.FestivalRunRepository;
 import com.oguzkaganmustafa.festivalmanager.repository.OrganiserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +15,8 @@ public class OrganiserService {
 
     @Autowired
     OrganiserRepository organiserRepository;
-
+    @Autowired
+    FestivalRunRepository festivalRunRepository;
 
     /**
      * It takes an organiser parameter and deliver it organiserRepository to save it to the database.
@@ -21,6 +24,13 @@ public class OrganiserService {
      * @return
      */
     public Organiser saveOrganiser(Organiser organiser){
+
+        for (FestivalRun festivalRun :
+                organiser.getFestivalRuns()) {
+            FestivalRun festivalRun1 = festivalRunRepository.getById(festivalRun.getFestRunId());
+            festivalRun1.getOrganisers().add(organiser);
+            festivalRunRepository.save(festivalRun1);
+        }
         return organiserRepository.save(organiser);
     }
 }
