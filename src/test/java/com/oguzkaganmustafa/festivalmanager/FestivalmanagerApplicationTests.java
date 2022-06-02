752a13c3786b5e095991f036c451f893a449ff29
GreatSum <42463375+oguzkaganaltas@users.noreply.github.com>
2022-06-02 17:04:56 +0300

    adding tests

--- src/test/java/com/oguzkaganmustafa/festivalmanager/FestivalmanagerApplicationTests.java
+++ src/test/java/com/oguzkaganmustafa/festivalmanager/FestivalmanagerApplicationTests.java
@@ -1,13 +1,27 @@
 package com.oguzkaganmustafa.festivalmanager;
 
+import com.oguzkaganmustafa.festivalmanager.entity.Concert;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
-class FestivalmanagerApplicationTests {
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = FestivalManagerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class FestivalmanagerApplicationTests{
+	@Autowired
+	TestRestTemplate restTemplate;
+
+	@LocalServerPort
+	int port;
 
 	@Test
-	void contextLoads() {
+	public void testConcertsByDescription()
+	{
+		assertTrue(this.restTemplate
+				.getForObject("http://localhost:" + port + "/concertsbydescription?key=Man", Concert.class)
+				.getDescription().contains("Man"));
 	}
-
 }
