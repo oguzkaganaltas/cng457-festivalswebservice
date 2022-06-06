9d0f434f735f4d14a1d15bcabb6bcb9d2860bbdf
Dexess <aygunmustafa999@gmail.com>
2022-06-06 11:06:23 +0300

    2 test

--- src/main/java/com/oguzkaganmustafa/festivalmanager/entity/Shows.java
+++ src/main/java/com/oguzkaganmustafa/festivalmanager/entity/Shows.java
@@ -1,5 +1,7 @@
 package com.oguzkaganmustafa.festivalmanager.entity;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +18,7 @@ import java.util.List;
 @NoArgsConstructor
 @Entity
 @Data
+
 public class Shows extends Event{
 
     @ManyToMany(mappedBy = "shows", fetch = FetchType.LAZY)
