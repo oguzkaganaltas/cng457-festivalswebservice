a66d9d85dffe2c2900d48a7fc6df889f0b32114b
Dexess <aygunmustafa999@gmail.com>
2022-06-12 10:56:47 +0300

    getallperformer implemented

--- src/main/java/com/oguzkaganmustafa/festivalmanager/controller/PerformerController.java
+++ src/main/java/com/oguzkaganmustafa/festivalmanager/controller/PerformerController.java
@@ -3,9 +3,12 @@ package com.oguzkaganmustafa.festivalmanager.controller;
 import com.oguzkaganmustafa.festivalmanager.entity.Performer;
 import com.oguzkaganmustafa.festivalmanager.service.PerformerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * PerformerController to manage web service of it.
@@ -25,5 +28,10 @@ public class PerformerController {
     public Performer addPerformer(@RequestBody Performer performer){
 
         return performerService.savePerformer(performer);
+    }
+
+    @GetMapping("/getallperformers")
+    public List<Performer> getAllPerformers(){
+        return performerService.getAllPerformers();
     }
 }
